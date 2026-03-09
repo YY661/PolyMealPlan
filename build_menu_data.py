9733c60d56ae83c91c1b4e347b5f413a2ab9a496
brand <btidd@calpoly.edu>
2026-03-08 21:26:38 -0700

    brandon_tidd

---
+++ build_menu_data.py
@@ -0,0 +1,48 @@
+from food_item import FoodItem
+import csv
+
+#csv values are read as strings, so we need to convert many of them (calories,fat,carbs,etc.) to floats
+def convert_csv_to_float(value:str) -> float | None:
+    if value is None:
+        return None
+    value = value.strip()
+    if value == "":
+        return None
+    try:
+        return float(value)
+    except ValueError:
+        return None
+
+#the dietary restrictions are stored as indicator variables in csv. We need to convert them to strings
+def convert_diet_restrictions(row:dict[str,str]) -> list[str]:
+    tags = []
+    csv_tags = ["VG","V","AG","HALAL"]
+    for tag in csv_tags:
+        if tag in row:
+            value = row[tag].strip()
+            if value == "1":
+                tags.append(tag)
+    return tags
+
+#convert each csv row, which is input as a dictionary, into FoodItem objects
+def convert_food_items(row:dict[str,str]) -> FoodItem:
+    item_name = row["item_name"].strip()
+    restaurant = row["restaurant_name"].strip()
+    calories = convert_csv_to_float(row["calories"])
+    fat = convert_csv_to_float(row["total_fat_g"])
+    carbs = convert_csv_to_float(row["total_carbs_g"])
+    protein = convert_csv_to_float(row["protein_g"])
+    diet_restrictions = convert_diet_restrictions(row)
+    return FoodItem(item_name,restaurant,calories,fat,carbs,protein,diet_restrictions)
+
+
+_converted = None
+
+#now, with helper functions, read csv dataset and convert each row into a FoodItem object in a list
+def get_data() -> list[FoodItem]:
+    global _converted
+    if _converted is None:
+        with open("menu_dataset", "r", encoding="utf-8") as dataset:
+            reader = csv.DictReader(dataset)
+            _converted = [convert_food_items(row) for row in reader]
+    return _converted
---
+++ build_menu_data_tests.py
@@ -0,0 +1,12 @@
+import food_item
+import build_menu_data
+
+reduced_data = [
+    build_menu_data.FoodItem("Waffle Potato Fries","Chick-fil-A",520,27,62,6,[]),
+    build_menu_data.FoodItem("Chicken Egg Roll","Panda Express",200,10,20,6,[]),
+    build_menu_data.FoodItem("Stir Fry Vegetables","Noodles",80,1.5,15,3,["VG","V"]),
+    build_menu_data.FoodItem("Green Beans","Noodles",60,3,5,2,["VG","V","AG"]),
+    build_menu_data.FoodItem("Toasted Farro","Hearth",130,0,26,5,["VG","V"]),
+    build_menu_data.FoodItem("Beef and Pork Meatballs","Hearth",680,50,20,36,[]),
+    build_menu_data.FoodItem("Beyond Burger","Brunch",560,29,52,27,["VG","V"])
+    ]
